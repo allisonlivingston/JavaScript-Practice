e30990a1b7002b368b4d117e2fd9443911447b21
Allison Livingston <livingstona16@gmail.com>
2018-10-09 09:28:54 -0600

    Created Pete-the-baker folder with app.js and README.md with problem and my solutions

---
+++ Pete-the-baker/app.js
@@ -0,0 +1,51 @@
+// -- SOLUTIONS --
+
+// MY FIRST SOLUTION
+
+function cakes(recipe, available) {
+  var availableToArray = Object.keys(available).map(function(key) {
+  return [key, available[key]]
+  })
+
+  var recipeToArray = Object.keys(recipe).map(function(key) {
+  return [key, recipe[key]]
+  })
+
+  var matchAvailableArray = recipeToArray.map(ingredient => ingredient[0]).map(ingredient => availableToArray.find(function(item) {
+    return item[0] == ingredient
+  }))
+
+  var matchRecipeArray = availableToArray.map(ingredient => ingredient[0]).map(ingredient => recipeToArray.find(function(item) {
+    return item[0] == ingredient
+  }))
+
+  var matchAvailableArrayFiltered = matchAvailableArray.filter(item => item !== undefined).sort()
+
+  var matchRecipeArrayFiltered = matchRecipeArray.filter(item => item !== undefined).sort()
+
+  let numberOfCakes = []
+
+  for(let i=0; i<matchAvailableArray.length; i++) {
+    if(matchAvailableArray[i] == undefined) {
+      return 0
+    } else {
+      numberOfCakes.push(Math.floor((matchAvailableArrayFiltered[i][1])/(matchRecipeArrayFiltered[i][1])))
+    }
+  }
+  return Math.min(...numberOfCakes)
+}
+
+// OTHER SOLUTION
+
+function cakes(recipe, available) {
+  return Object.keys(recipe).reduce(function(val, ingredient) {
+    return Math.min(Math.floor(available[ingredient] / recipe[ingredient] || 0), val)
+  }, Infinity)
+}
+
+// OTHER SOLUTION
+
+function cakes(recipe, available) {
+  return Math.min(
+  ...Object.keys(recipe).map(key => Math.floor(available[key] / recipe[key] || 0))
+)}
