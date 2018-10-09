74b82552e828c98e759e85b3c7384e5eb3652730
Allison Livingston <livingstona16@gmail.com>
2018-10-09 09:39:24 -0600

    Added refactored solution to app.js file

--- Pete-the-baker/app.js
+++ Pete-the-baker/app.js
@@ -35,6 +35,37 @@ function cakes(recipe, available) {
   return Math.min(...numberOfCakes)
 }
 
+// REFACTORED SOLUTION
+
+function cakes(recipe, available) {
+  var availableToArray = Object.keys(available).map(function(key) {
+  return [key, available[key]]
+  })
+
+  var recipeToArray = Object.keys(recipe).map(function(key) {
+  return [key, recipe[key]]
+  })
+
+  var matchAvailableArray = recipeToArray.map(ingredient => ingredient[0]).map(ingredient => availableToArray.find(function(item) {
+    return item[0] == ingredient
+  })).sort()
+
+  var matchRecipeArray = availableToArray.map(ingredient => ingredient[0]).map(ingredient => recipeToArray.find(function(item) {
+    return item[0] == ingredient
+  })).sort()
+
+  let numberOfCakes = []
+
+  for(let i=0; i<matchAvailableArray.length; i++) {
+    if(matchAvailableArray[i] == undefined) {
+      return 0
+    } else {
+      numberOfCakes.push(Math.floor((matchAvailableArray[i][1])/(matchRecipeArray[i][1])))
+    }
+  }
+  return Math.min(...numberOfCakes)
+}
+
 // OTHER SOLUTION
 
 function cakes(recipe, available) {
