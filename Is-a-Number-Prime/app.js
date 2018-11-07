19d38a2361c8d36ed5f1ffa667c76b7390817919
Allison Livingston <livingstona16@gmail.com>
2018-11-07 08:50:36 -0700

    Created Is-a-Number-Prime folder with app.js and README.md files with problem and my solution

---
+++ Is-a-Number-Prime/app.js
@@ -0,0 +1,27 @@
+// -- SOLUTIONS --
+
+// MY FIRST SOLUTION
+
+function isPrime(num) {
+  if(num === 0 || num === 1 || num < 0) {
+    return false
+  } else {
+    let compositeNumbers = 0
+    for(let i=2; i<num; i++) {
+      num % i === 0 ? compositeNumbers += 1 : compositeNumbers += 0
+    }
+    return compositeNumbers === 0 ? true : false
+  }
+}
+
+// REFACTORED SOLUTION
+
+function isPrime(num) {
+  var prime = true;
+  for(var i = 2; i < num; i++) {
+    if(num % i === 0) {
+      prime = false;
+    }
+  }
+  return (num < 2) ? false : prime;
+}
